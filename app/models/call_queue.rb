43eeabb981213b676336ea9a53c1e2b436752a98
Ilia Zhirov <izhirov@gmail.com>
2012-09-20 14:17:01 +0600

    add callqueue model

---
+++ app/models/call_queue.rb
@@ -0,0 +1,4 @@
+class CallQueue < ActiveRecord::Base
+  attr_accessible :announce, :announce_frequency, :announce_holdtime, :announce_round_seconds, :eventmemberstatus, :eventwhencalled, :joinempty, :leavewhenempty, :maxlen, :memberdelay, :monitor_format, :monitor_join, :musiconhold, :name, :queue_callswaiting, :queue_holdtime, :queue_lessthan, :queue_minutes, :queue_reporthold, :queue_seconds, :queue_thankyou, :queue_thereare, :queue_youarenext, :reportholdtime, :retry, :servicelevel, :strategy, :timeout, :timeoutrestart, :weight, :wrapuptime
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+end
---
+++ db/migrate/20120920060449_create_call_queues.rb
@@ -0,0 +1,39 @@
+class CreateCallQueues < ActiveRecord::Migration
+  def change
+    create_table :call_queues, force: true do |t|
+      t.string :name, limit: 128
+      t.string :musiconhold, limit: 128
+      t.string :announce, limit: 128
+      t.integer :timeout, limit: 11
+      t.boolean :monitor_join
+      t.string :monitor_format, limit: 128
+      t.string :queue_youarenext, limit: 128
+      t.string :queue_thereare, limit: 128
+      t.string :queue_callswaiting, limit: 128
+      t.string :queue_holdtime, limit: 128
+      t.string :queue_minutes, limit: 128
+      t.string :queue_seconds, limit: 128
+      t.string :queue_lessthan, limit: 128
+      t.string :queue_thankyou, limit: 128
+      t.string :queue_reporthold, limit: 128
+      t.integer :announce_frequency, limit: 11
+      t.integer :announce_round_seconds, limit: 11
+      t.string :announce_holdtime, limit: 128
+      t.integer :retry, limit: 11
+      t.integer :wrapuptime, limit: 11
+      t.integer :maxlen, limit: 11
+      t.integer :servicelevel, limit: 11
+      t.string :strategy, limit: 128
+      t.string :joinempty, limit: 128
+      t.string :leavewhenempty, limit: 128
+      t.boolean :eventmemberstatus
+      t.boolean :eventwhencalled
+      t.boolean :reportholdtime
+      t.integer :memberdelay, limit: 11
+      t.integer :weight, limit: 11
+      t.boolean :timeoutrestart
+
+      t.timestamps
+    end
+  end
+end

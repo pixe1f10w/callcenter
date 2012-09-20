d0759fcaade25f098d61c87ba5ba1616a6890be3
Ilia Zhirov <izhirov@gmail.com>
2012-09-20 11:54:40 +0600

    add validations

--- app/models/extension.rb
+++ app/models/extension.rb
@@ -1,3 +1,8 @@
 class Extension < ActiveRecord::Base
   attr_accessible :app, :appdata, :context, :exten, :priority
+  validates :context, presence: true
+  validates :exten, presence: true
+  validates :priority, presence: true
+  validates :app, presence: true
+  validates :appdata, presence: true
 end

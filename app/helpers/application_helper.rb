6ede55377406bc47c114a080eac190b9c3af277b
Ilia Zhirov <izhirov@gmail.com>
2012-10-23 12:43:02 +0600

    basic bootstrap-powered template implemented

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # returns the full title on a per-page basis
+  def full_title page_title
+    title = "Callcenter"
+    title << " | #{page_title}" unless page_title.empty?
+    title
+  end
 end

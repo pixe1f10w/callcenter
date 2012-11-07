f3bc3db301ded64bfcb7a79d55f27dab2a76fdf5
Ilia Zhirov <izhirov@gmail.com>
2012-11-07 14:16:15 +0600

    created at made visible

--- app/models/call.rb
+++ app/models/call.rb
@@ -1,4 +1,4 @@
 class Call < ActiveRecord::Base
-  attr_accessible :uniqueid, :ended_at, :answered_at, :joined_at, :from, :to, :duration, :inbound
+  attr_accessible :uniqueid, :created_at, :ended_at, :answered_at, :joined_at, :from, :to, :duration, :inbound
   set_primary_key :uniqueid
 end
